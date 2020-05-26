9984623fcce2fbeaa55118984adba792d8050f8e
NicuD <nicolae.dicu@wipro.com>
2020-05-26 17:16:53 +0300

    Add broadcast

--- app/src/main/java/com/nicu1989/tweepy/MainActivity.java
+++ app/src/main/java/com/nicu1989/tweepy/MainActivity.java
@@ -1,13 +1,22 @@
 package com.nicu1989.tweepy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.app.DownloadManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -43,25 +52,19 @@ public class MainActivity extends AppCompatActivity {
     TextView txtJson;
     String url;
     boolean jobScheduled = false;
-    String jsonData;
+    //String jsonData;
+    LinearLayout lLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout lLayout = (LinearLayout) findViewById(R.id.linear);
+        lLayout = (LinearLayout) findViewById(R.id.linear);
+    }
 
-        String requestUrl = "http://192.168.0.145:3000/tweeter";
-
-        try{
-            Intent intent = getIntent();
-            jobScheduled = intent.getBooleanExtra("jobScheduled", false);
-            jsonData = intent.getStringExtra("json");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        if (jobScheduled){
+    private void parseJson(String jsonData) {
+        if (true){
             Button startMonitor = findViewById(R.id.button);
             //startMonitor.setClickable(false);
             JSONArray jsonArr1 = null;
@@ -83,10 +86,11 @@ public class MainActivity extends AppCompatActivity {
 
             try {
                 jsonArr2 = jsonObj1.getJSONArray("tweets");
+                //jsonObj3 = jsonObj1.getJSONObject("newTweets");
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-
+            lLayout.removeAllViews();
             for(int n = 0; (n < (jsonArr2.length()) && (n < 10)); n++)
             {
                 try {
@@ -108,22 +112,62 @@ public class MainActivity extends AppCompatActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                // do some stuff....
+            }
+
+            try {
+                if(jsonObj1.getBoolean("newTweets")){
+                    show_Notification();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
 
             Log.d("MyApp", jsonObj2.toString());
         }
+    }
 
-        /*final Handler handler = new Handler();
-        handler.post(new Runnable() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+
             @Override
-            public void run() {
-                handler.postDelayed(this, 6000);
-                new JsonTask().execute(url);
-                Toast.makeText(getApplicationContext(), "AsyncTask runs every 6 seconds", Toast.LENGTH_SHORT).show();
+            public void onReceive(Context context, Intent intent) {
+           Toast.makeText(context, "Message is: "+ intent.getStringExtra("message"), Toast.LENGTH_LONG)
+                    .show();
+                String action = intent.getAction();
+                switch (action) {
+                    case "msg":
+                        String mess = intent.getStringExtra("message");
+                        parseJson(mess);
+                        break;
+                }
             }
-        });*/
 
+        };
+
+        IntentFilter filter = new IntentFilter("msg");
+        registerReceiver(mBroadcastReceiver,filter);
+    }
+
+    public void show_Notification(){
+
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+        String CHANNEL_ID="MYCHANNEL";
+        NotificationChannel notificationChannel=new NotificationChannel(CHANNEL_ID,"name", NotificationManager.IMPORTANCE_LOW);
+        PendingIntent pendingIntent=PendingIntent.getActivity(getApplicationContext(),1,intent,0);
+        Notification notification=new Notification.Builder(getApplicationContext(),CHANNEL_ID)
+                .setContentText("TweepyApp")
+                .setContentTitle("You have new tweets!")
+                .setContentIntent(pendingIntent)
+                .addAction(android.R.drawable.sym_action_chat,"TitleX",pendingIntent)
+                .setChannelId(CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.sym_action_chat)
+                .build();
+
+        NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(notificationChannel);
+        notificationManager.notify(1,notification);
     }
 
     private void setTextView(LinearLayout lLayout, JSONArray jsonArr2, int n, String text, int color, boolean tweet) throws JSONException {
@@ -159,82 +203,16 @@ public class MainActivity extends AppCompatActivity {
 
         JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
         int resultCode = scheduler.schedule(info);
+
         if (resultCode == JobScheduler.RESULT_SUCCESS) {
             Log.d(TAG, "Job scheduled");
         } else {
             Log.d(TAG, "Job scheduling failed");
         }
     }
-
-    /*private class JsonTask extends AsyncTask<String, String, String> {
-
-        protected void onPreExecute() {
-            super.onPreExecute();
-
-            pd = new ProgressDialog(MainActivity.this);
-            pd.setMessage("Please wait");
-            pd.setCancelable(false);
-            pd.show();
-        }
-
-        protected String doInBackground(String... params) {
-
-
-            HttpURLConnection connection = null;
-            BufferedReader reader = null;
-
-            try {
-                URL url = new URL(params[0]);
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-
-
-                InputStream stream = connection.getInputStream();
-
-                reader = new BufferedReader(new InputStreamReader(stream));
-
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line+"\n");
-                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
-
-                }
-
-                return buffer.toString();
-
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (connection != null) {
-                    connection.disconnect();
-                }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            if (pd.isShowing()){
-                pd.dismiss();
-            }
-            txtJson = findViewById(R.id.tv1);
-            txtJson.setText(result);
-        }
-    }*/
 }
+
+
 
 
 
--- app/src/main/java/com/nicu1989/tweepy/Scheduler.java
+++ app/src/main/java/com/nicu1989/tweepy/Scheduler.java
@@ -1,9 +1,16 @@
 package com.nicu1989.tweepy;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -55,11 +62,12 @@ public class Scheduler extends JobService {
                         Log.d("Response: ", "> " + line);
                     }
 
-                    Intent activ = new Intent(getBaseContext().getApplicationContext(), MainActivity.class);
-                    activ.putExtra("jobScheduled", true);
-                    activ.putExtra("json", buffer.toString());
-                    activ.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(activ);
+                    Intent intent = new Intent("msg");    //action: "msg"
+                    intent.setPackage(getPackageName());
+                    intent.putExtra("message", buffer.toString());
+                    getApplicationContext().sendBroadcast(intent);
+
+
 
 
                 } catch (MalformedURLException e) {
@@ -83,6 +91,8 @@ public class Scheduler extends JobService {
             }
         }).start();
     }
+
+
 
     public String getJson(){
         return buffer.toString();
